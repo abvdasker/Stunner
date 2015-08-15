1118a0b44a3fd9f472e32126c35b277a0b62e067
abvdasker <hal.c.rogers@gmail.com>
2015-08-15 17:06:45 -0400

    wip adds mapped value parsing

--- src/main/java/com/hal/stun/message/attribute/value/MappedAddressStunAttributeValue.java
+++ src/main/java/com/hal/stun/message/attribute/value/MappedAddressStunAttributeValue.java
@@ -1,8 +1,8 @@
-package hal.com.stun.message.attribute.value;
+package com.hal.stun.message.attribute.value;
 
 public class MappedAddressAttributeValue {
   
-  
+  7 bytes OR 19 bytes
   // 0x0001 - type;
   // 0x0000 - length;
   // value
@@ -12,17 +12,47 @@ public class MappedAddressAttributeValue {
   //     OR
   //   0x0000 0000 0000 0000 0000 0000 0000 0000 ipv6 address
   //
+  private static final byte IPV4_FAMILY_CODE = (byte) 0x01;
+  private static final byte IPV6_FAMILY_CODE = (byte) 0x02;
+  
+  private static final int IPV4_ATTRIBUTE_SIZE = 7;
+  private static final int IPV6_ATTRIBUTE_SIZE = 19;
+  
   private byte addressFamily;
   private short port;
   private String addressHex;
   
   public MappedAddressAttributeValue(String valueHex) {
     super(valueHex);
-    parseValueHex(valueHex);
+    parseValueBytes();
   }
   
-  private parseValueHex(String valueHex) {
+  private parseValueBytes() {
+    addressFamily = valueBytes[1];
+    port = parsePort();
+    addressHex = parseAddressHex();
+  }
+  
+  private short parsePort() {
+    int thisPort = 0;
+    thisPort = valueBytes[2];
+    thisPort <<= 8;
+    thisPort |= valueBytes[3];
+    return thisPort;
+  }
+  
+  private String parseAddressHex() {
     
+  }
+  
+  private boolean isValid() {
+    if (addressFamily == IPV4_FAMILY_CODE && valueBytes.length == IPV4_ATTRIBUTE_SIZE) {
+      return true;
+    } else if (addressFamily == IPV6_FAMILY_CODE && valueBytes.length == IPV6_ATTRIBUTE_SIZE) {
+      return true;
+    }
+    
+    return false;
   }
   
 }
--- src/main/java/com/hal/stun/message/attribute/value/StunAttributeValue.java
+++ src/main/java/com/hal/stun/message/attribute/value/StunAttributeValue.java
@@ -1,11 +1,23 @@
 package com.hal.stun.message.attribute.value;
 
+import com.hal.message.StunMessageUtils;
+
 public abstract class StunAttributeValue {
   
   private String attributeValueHex;
+  protected byte[] valueBytes;
   public StunAttributeValue(String attributeValueHex) {
     this.attributeValueHex = attributeValueHex;
+    this.valueBytes = StunMessageUtils.convertHexToByteArray(valueHex);
+    validate();
   }
   
+  private void validate() throws StunParseException {
+    if (!isValid()) {
+      throw new StunParseException("could not parse attribute value " + attributeValueHex);
+    }
+  }
+  
+  private abstract boolean isValid();
   
 }
