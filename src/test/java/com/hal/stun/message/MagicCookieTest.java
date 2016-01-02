0f687f113002c82b0b6c41015a6982a23cc8c260
abvdasker <hal.c.rogers@gmail.com>
2016-01-02 17:54:28 -0500

    Adds specs of magic cookie

--- src/test/java/com/hal/stun/message/MagicCookieTest.java
+++ src/test/java/com/hal/stun/message/MagicCookieTest.java
@@ -1,10 +1,28 @@
 package com.hal.stun.message;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class MagicCookieTest {
   @Test(expected = IndexOutOfBoundsException.class)
-  public void getByteTooLarge() throws IndexOutOfBoundsException {
-    MagicCookie.getByte(5);
+  public void testGetByteTooLarge() throws IndexOutOfBoundsException {
+    MagicCookie.getByte(4);
+  }
+
+  @Test
+  public void testGetByte() {
+    byte expectedByte = (byte) 0x12;
+    byte actualByte = MagicCookie.getByte(2);
+
+    Assert.assertEquals("correctly extracts magic cookie byte 2", expectedByte, actualByte);
+  }
+
+  @Test
+  public void testGetTyopTwoBytes() {
+    short expected = (short) 0x2112;
+    short actual = MagicCookie.getTopTwoBytes();
+
+    Assert.assertEquals("correctly extracts top two bytes as short",
+                        expected, actual);
   }
 }
