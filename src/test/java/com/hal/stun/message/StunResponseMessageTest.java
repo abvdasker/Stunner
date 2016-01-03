903d939f992881fbd8b4a02b316ab5614c301926
abvdasker <hal.c.rogers@gmail.com>
2016-01-02 20:01:21 -0500

    re-enables response message test

--- src/test/java/com/hal/stun/message/StunResponseMessageTest.java
+++ src/test/java/com/hal/stun/message/StunResponseMessageTest.java
@@ -12,6 +12,6 @@ public class StunResponseMessageTest {
                                           StunMessageTestData.getRealStunMessageBytes(),
                                           requestAddress);
 
-    // StunResponseMessage response = new StunResponseMessage(request);
+    StunResponseMessage response = new StunResponseMessage(request);
   }
 }
