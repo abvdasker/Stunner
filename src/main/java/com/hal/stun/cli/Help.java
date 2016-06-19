ee3f676456255ee2282eb12791402f71356968ea
abvdasker <hal.c.rogers@gmail.com>
2016-06-18 21:36:19 -0400

    Implement string joiner for old java compatibility

--- src/main/java/com/hal/stun/cli/Help.java
+++ src/main/java/com/hal/stun/cli/Help.java
@@ -12,7 +12,17 @@ public class Help {
   private static final String HELP_HEADER = "\nUsage:\n\n$> java -jar Stunner.jar\n";
 
   public static String getHelpText() {
-    String joinedHelpText = String.join("\n", HELP_TEXT);
+    String joinedHelpText = join("\n", HELP_TEXT);
     return HELP_HEADER + joinedHelpText;
+  }
+
+  private static String join(String inter, String[] strings) {
+    StringBuffer joinedString = new StringBuffer();
+    for (int i = 0; i < strings.length - 1; i++) {
+      joinedString.append(strings[i]);
+      joinedString.append(inter);
+    }
+    joinedString.append(strings[strings.length - 1]);
+    return joinedString.toString();
   }
 }
