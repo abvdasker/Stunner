40b11f4e19ef40036ed82b0cf380c5fc6054789c
abvdasker <hal.c.rogers@gmail.com>
2016-10-23 19:34:54 -0400

    Adds test of stun attribute value

---
+++ src/test/java/com/hal/stun/message/attribute/value/StunAttributeValueTest.java
@@ -0,0 +1,43 @@
+package com.hal.stun.message.attribute.value;
+
+import com.hal.stun.message.StunParseException;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class StunAttributeValueTest {
+
+  @Test
+  public void testGetPaddedBytes() throws Exception {
+    byte[] testBytes = {
+      (byte) 1,
+      (byte) 2,
+      (byte) 3,
+      (byte) 4,
+      (byte) 5
+    };
+    StunAttributeValue value = new TestStunAttributeValue(testBytes);
+    byte[] paddedBytes = value.getPaddedBytes();
+
+    for (int i = 0; i < testBytes.length; i++) {
+      Assert.assertEquals(testBytes[i], paddedBytes[i]);
+    }
+    for (int i = 5; i < 8; i++) {
+      Assert.assertEquals(0, paddedBytes[i]);
+    }
+  }
+
+  private static class TestStunAttributeValue extends StunAttributeValue {
+    public TestStunAttributeValue(byte[] value) throws StunParseException {
+      super(value);
+    }
+
+    protected void parseValueBytes() throws StunParseException {
+    }
+
+    protected boolean isValid() {
+      return true;
+    }
+  }
+
+}
