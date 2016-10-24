bba21b801c2fe508a222c8aaca6f2fe5913558b5
abvdasker <hal.c.rogers@gmail.com>
2016-10-23 20:06:21 -0400

    Adds spec of software attribute data parsing

--- src/test/java/com/hal/stun/message/attribute/value/SoftwareStunAttributeValueTest.java
+++ src/test/java/com/hal/stun/message/attribute/value/SoftwareStunAttributeValueTest.java
@@ -11,18 +11,30 @@ import com.hal.stun.message.StunParseException;
 public class SoftwareStunAttributeValueTest {
 
   private static final byte[] SOFTWARE_ATTRIBUTE_VALUE
-    = Arrays.copyOfRange(ClientTestData.BASIC_REQUEST_IPV4, 24, 41);
+    = Arrays.copyOfRange(ClientTestData.BASIC_REQUEST_IPV4, 24, 40);
   private static final String MESSAGE_TOO_LONG
     = "irEgQIne11fRWvb9wGuEKYJtxruiLp5mTnxS4KZ22w6oerHUZ46IFjwXe2lOBY" +
     "hWfNB4Elb9DD4DHLMzykXTlUKkSOpB2an39NONzfHGyQ1yjonpE08Y5N4zyYmtvu4R";
 
   @Test
-  public void testInitialize() throws Exception {
+  public void testInitialize() {
     try {
       new SoftwareStunAttributeValue(SOFTWARE_ATTRIBUTE_VALUE);
     } catch (Exception exception) {
       Assert.fail();
     }
+  }
+
+  @Test
+  public void testParseMessage() throws Exception {
+    String expectedMessage = "STUN test client";
+
+    SoftwareStunAttributeValue value = new SoftwareStunAttributeValue(SOFTWARE_ATTRIBUTE_VALUE);
+    String message = value.toString();
+
+    Assert.assertEquals("correctly parses the software name from byte array",
+                        expectedMessage,
+                        message);
   }
 
   @Test(expected = StunParseException.class)
